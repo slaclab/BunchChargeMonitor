e9d5c970c024876221fd410f0423b441d1be94b2
Marcio Paduan Donadio <marcio@slac.stanford.edu>
2017-03-14 22:31:44 -0700

    BCM separated in to pieces, one for Toroid and other for Faraday Cup

--- iocBoot/vioc-li00-im02/st.cmd
+++ iocBoot/vioc-li00-im02/st.cmd
@@ -1,9 +1,10 @@
 #!../../bin/linuxRT-x86_64/bcm
 
 #########################################
-# This IOC is related to the Faraday Cup
+# This IOC is related to the Toroid
 # for the Bunch Charge Monitor.
 #########################################
+
 
 ## You may have to change bcm to something else
 ## everywhere it appears in this file
@@ -29,38 +30,28 @@
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "21000000")
 
 # PV prefix
-epicsEnvSet("PREFIX","TORO:LI00:IM01")
-epicsEnvSet("CPSW_PORT1","Atca6")
+epicsEnvSet("PREFIX","TORO:LI00:IM02")
+epicsEnvSet("CPSW_PORT2","Atca7")
 
+
+# *****************************************************
+# **** Environment variables for Toroid on  Bergoz ****
+
+epicsEnvSet("P_BERGOZ","$(P=TORO:)")
+epicsEnvSet("R","$(R=LI00:IM02:)")
+epicsEnvSet("BERGOZ_PORT","$(PORT=L0)")
+epicsEnvSet("BERGOZ_TTY","$(BERGOZ_TTY=/dev/ttyACM0)")
+epicsEnvSet("SERIALNUM_EXPECT","$(SERIALNUM_EXPECT=40)")
 
 # Temperature xfer: ESLO, EOFF
 epicsEnvSet("ESLO","$(ESLO=0.01)")
 epicsEnvSet("EOFF","$(EOFF=273.15)")
 
 
-# ***********************************************************
-# **** Environment variables for Faraday Cup on Keithley ****
-
-epicsEnvSet("P_KEITHLEY","$(P=FARC:)")
-#epicsEnvSet("R","LI0:BP01:")
-epicsEnvSet("K6487_ADDRESS","$(K6487_ADDRESS=ts-b084-nw01:2110)")
-epicsEnvSet("K6487_PROTOCOL","$(K6487_PROTOCOL=TCP)")
-epicsEnvSet("STREAM_PROTOCOL_PATH","${TOP}/db")
-
-
-# *******************************************************************
-# **** Environment variables for Temperature Chassis on Ethercat ****
-
-# System Location:
-epicsEnvSet(FAC,"SYS0")
-epicsEnvSet("LOCA","LI00")
-epicsEnvSet("TEMP_IOC_NAME","TEMP:${LOCA}:IM01")
-
-
 # *********************************************
 # **** Environment variables for IOC Admin ****
 
-epicsEnvSet(IOC_NAME,"TORO:LI00:IM01")
+epicsEnvSet(IOC_NAME,"TORO:LI00:IM02")
 
 
 cd ${TOP}
@@ -88,30 +79,18 @@ bcm_registerRecordDeviceDriver(pdbbase)
 #    IP Address,                # OPTIONAL: Target FPGA IP Address. If not given it is taken from the YAML file
 #    Record name Prefix,        # Record name prefix
 #    Record name Length Max,    # Record name maximum length (must be greater than lenght of prefix + 4)
-cd ${TOP}/iocBoot/vioc-li00-im01
+cd ${TOP}/iocBoot/vioc-li00-im02
 # In Sector 0 L2KA00-05, the BCMs are in slots 6 and 7. Here, for testing purposes we are using slots 4 and 5.
-YCPSWASYNConfig("${CPSW_PORT1}", "${TOP}/yaml/AmcCarrierBcm_project.yaml/000TopLevel.yaml", "", "10.0.1.104", "${PREFIX}", 40)
+YCPSWASYNConfig("${CPSW_PORT2}", "${TOP}/yaml/AmcCarrierBcm_project.yaml/000TopLevel.yaml", "", "10.0.1.105", "${PREFIX}", 40)
 
 cd ${TOP}
 
+# *********************************
+# **** Driver setup for Bergoz ****
 
-# ***********************************
-# **** Driver setup for Keithley ****
-
+# Set up ASYN ports
 # drvAsynIPPortConfigure port ipInfo priority noAutoconnect noProcessEos
-drvAsynIPPortConfigure("L1","$(K6487_ADDRESS)",0,0,0)
-
-
-# **********************************************************
-# **** Driver setup for Temperature Chassis on Ethercat ****
-
-# Init EtherCAT: To support Real Time fieldbus
-# EtherCAT AsynDriver must be initialized in the IOC startup script before iocInit
-# ecAsynInit("<unix_socket>", <max_message>)
-# unix_socket = path to the unix socket created by the scanner
-# max_message = maximum size of messages between scanner and ioc
-
-ecAsynInit("/tmp/sock1", 1000000)
+drvAsynSerialPortConfigure("$(BERGOZ_PORT)","$(BERGOZ_TTY)",0,0,0)
 
 
 # ===========================================
@@ -124,11 +103,11 @@ ecAsynInit("/tmp/sock1", 1000000)
 #asynSetTraceMask(${PORT},, -1, 9)
 
 
-# *********************************
-# **** Asyn Masks for Keithley ****
+# *******************************
+# **** Asyn Masks for Bergoz ****
 
-#asynSetTraceIOMask("L1",-1,0x2)
-#asynSetTraceMask("L1",-1,0x9)
+#asynSetTraceIOMask("$(BERGOZ_PORT)",-1,0x2)
+#asynSetTraceMask("$(BERGOZ_PORT)",-1,0x9)
 
 
 # ===========================================
@@ -142,20 +121,13 @@ ecAsynInit("/tmp/sock1", 1000000)
 dbLoadRecords("db/verifyDefaults.db", "P=${PREFIX}, KEY=3")
 
 
+# ************************
+# **** Load Bergoz db ****
 
-# **************************
-# **** Load Keithley db ****
-dbLoadRecords ("db/devKeithley6487.db" "P=$(P_KEITHLEY),R=$(R),PORT=L1,A=-1,NELM=1000")
-dbLoadRecords ("db/asynRecord.db" "P=$(P_KEITHLEY),R=$(R),PORT=L1,ADDR=-1,OMAX=0,IMAX=0")
+dbLoadRecords("db/devBergozBCM.db" "P=$(P_BERGOZ),R=$(R),PORT=$(BERGOZ_PORT),A=-1")
+dbLoadRecords("db/asynRecord.db" "P=$(P_BERGOZ),R=asyn,PORT=$(BERGOZ_PORT),ADDR=-1,OMAX=0,IMAX=0")
+dbLoadRecords("db/TempMonitoring_TORO.db", "P=$(P_BERGOZ)$(R),ESLO=$(ESLO),EOFF=$(EOFF)")
 
-
-# *****************************************************
-# **** Load db for Temperature Chassis on Ethercat ****
-
-# Load the database templates for the EtherCAT components
-# dbLoadRecords("db/<template_name_for slave_module>, <pass_in_macros>)
-dbLoadRecords("db/EK1101.template", "DEVICE=TEMP:LI00:,PORT=COUPLER0,SCAN=1 second")
-dbLoadRecords("db/EL3202-0010.template", "DEVICE=TEMP:LI00,PORT=ANALOGINPUT,SCAN=1 second")
 
 
 # **********************************************************************
@@ -248,7 +220,12 @@ create_monitor_set("info_settings.req" , 30 )
 dbpf ${PREFIX}:C:A24LC64FT:MemoryArray:Rd.SCAN "Passive"
 
 
-# ************************************************************
-# **** System command for Temperature Chassis on Ethercat ****
-# Setup Real-time priorities after iocInit for driver threads
-system("/bin/su root -c `pwd`/rtPrioritySetup.cmd")
+# *********************
+# **** Bergoz dbpf ****
+
+# Save the TTY device name
+dbpf $(P_BERGOZ)$(R)TTY_RD $(BERGOZ_TTY)
+
+# Save the expected BERGOZ serial number
+dbpf $(P_BERGOZ)$(R)SERIALNUM_EXPECT $(SERIALNUM_EXPECT)
+
