8b480ef89848130185b06e3d618e1f2e7c3e4dac
Chris Ford <caf@slac.stanford.edu>
2017-04-03 18:15:42 -0700

    EDL files, initial commit.

---
+++ bcmApp/srcDisplay/startEdm_FC.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+edm -x -m "prefix=FARC:LI00:215,carrier_prefix=AMCC:LI00:14,MAD=BCM-FC" bcmFcTop.edl &
---
+++ bcmApp/srcDisplay/startEdm_toroid.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+edm -x -m "prefix=TORO:LI00:215,carrier_prefix=AMCC:LI00:15,MAD=BCM-Toroid" bcmBergozTop.edl &
