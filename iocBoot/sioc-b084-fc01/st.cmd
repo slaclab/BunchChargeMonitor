fb2e2c7ebcf75b4711510a133bc6c274323f3bc1
Jesus Vasquez <jvasquez@slac.stanford.edu>
2017-11-21 15:19:51 -0800

    Update saveLoadConfig to new version

--- iocBoot/sioc-b084-fc01/st.cmd
+++ iocBoot/sioc-b084-fc01/st.cmd
@@ -177,7 +177,7 @@ drvAsynIPPortConfigure("$(K6482_PORT)","$(K6482_ADDRESS)",0,0,0)
 # **** Load YCPSWAsyn db ****
 
 #Save/Load configuration related records
-dbLoadRecords("db/saveLoadConfig.db", "P=${AMC_CARRIER_PREFIX}, PORT=${CPSW_PORT}, SAVE_FILE=/tmp/configDump.yaml, LOAD_FILE=yaml/EPICS_defaultsSine_FC10-28-17.yaml")
+dbLoadRecords("db/saveLoadConfig.db", "P=${AMC_CARRIER_PREFIX}, PORT=${CPSW_PORT}, SAVE_FILE=/tmp/configDump.yaml, LOAD_FILE=yaml/defaultsSine_FC10-28-17.yaml, SAVE_ROOT=mmio, LOAD_ROOT=mmio")
 
 # Manually create records
 dbLoadRecords("db/bcm.db", "P=${AMC0_PREFIX}, PORT=${CPSW_PORT}, AMC=0")
