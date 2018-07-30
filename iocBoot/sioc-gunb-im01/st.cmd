38e44e68c0adbe2336856c02afb08d7a47740223
Dan Sciortino <disco@slac.stanford.edu>
2018-07-30 11:34:50 -0700

    changed bergoz path script

--- iocBoot/sioc-gunb-im01/st.cmd
+++ iocBoot/sioc-gunb-im01/st.cmd
@@ -71,7 +71,7 @@ epicsEnvSet(IOC_NAME,"SIOC:$(AREA):IM01")
 # **** Environment variables for bergoz detection  ****
 # Serial number for bergoz
 epicsEnvSet("IM01","0x2c")
-< ./usr/local/lcls/epics/iocCommon/iocSpecificRelease/iocboot/sioc-gunb-im01/getBergozLocation.sh
+< .${EPICS_IOC_APP}/getBergozLocation.sh
 
 
 # *****************************************************
