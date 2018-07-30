532a09f35b41510cacbf13333dcda35997df6ff7
Dan Sciortino <disco@slac.stanford.edu>
2018-07-30 12:51:12 -0700

    added missing quotes

--- iocBoot/sioc-gunb-im01/st.cmd
+++ iocBoot/sioc-gunb-im01/st.cmd
@@ -71,7 +71,7 @@ epicsEnvSet(IOC_NAME,"SIOC:$(AREA):IM01")
 # **** Environment variables for bergoz detection  ****
 # Serial number for bergoz
 epicsEnvSet("IM01","0x2c")
-system(${EPICS_IOC_APP}/getBergozLocation.sh)
+system("${EPICS_IOC_APP}/getBergozLocation.sh")
 
 
 # *****************************************************
