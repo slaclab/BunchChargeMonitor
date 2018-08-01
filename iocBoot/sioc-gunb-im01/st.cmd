c7e945dc4d7ae4608383751066b04d312fc97f05
Dan Sciortino <disco@slac.stanford.edu>
2018-08-01 14:32:16 -0700

    updated ethercat device name

--- iocBoot/sioc-gunb-im01/st.cmd
+++ iocBoot/sioc-gunb-im01/st.cmd
@@ -216,9 +216,9 @@ dbLoadRecords("db/TempMonitoring_TORO.db", "P=$(BERGOZ0_P)$(BERGOZ0_R),ESLO=$(ES
 # Load the database templates for the EtherCAT components
 # dbLoadRecords("db/<template_name_for slave_module>, <pass_in_macros>)
 dbLoadRecords("db/EK1101.template", "DEVICE=${TEMP_IOC_NAME}:BCM_EK1101,PORT=COUPLER0,SCAN=1 second")
-dbLoadRecords("db/EL3202-0010.template", "DEVICE=${TEMP_IOC_NAME}:BCM_EL3202_1,PORT=Node1,SCAN=1 second")
-dbLoadRecords("db/EL3202-0010.template", "DEVICE=${TEMP_IOC_NAME}:BCM_EL3202_2,PORT=Node2,SCAN=1 second")
-dbLoadRecords("db/EL3202-0010.template", "DEVICE=${TEMP_IOC_NAME}:BCM_EL3202_3,PORT=Node3,SCAN=1 second")
+dbLoadRecords("db/EL3202-0010.template", "DEVICE=${TEMP_IOC_NAME}:TEMP1,PORT=Node1,SCAN=1 second")
+dbLoadRecords("db/EL3202-0010.template", "DEVICE=${TEMP_IOC_NAME}:TEMP2,PORT=Node2,SCAN=1 second")
+dbLoadRecords("db/EL3202-0010.template", "DEVICE=${TEMP_IOC_NAME}:TEMP3,PORT=Node3,SCAN=1 second")
 
 # ****************************
 # **** Load BSA driver DB ****
