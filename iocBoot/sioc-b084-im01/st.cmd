5210ef9cd00c7de14416fb0069dc4e81e36d6c25
carolina <carolina@slac.stanford.edu>
2018-10-11 14:24:28 -0700

    changed the dbLoadrecords macro to LOAD_FILE=yaml/defaultsToro08-21-18.yaml

--- iocBoot/sioc-b084-im01/st.cmd
+++ iocBoot/sioc-b084-im01/st.cmd
@@ -182,7 +182,7 @@ tprTriggerAsynDriverConfigure("trig", "mmio/AmcCarrierCore")
 # ***********************************************************************
 # **** Load YCPSWAsyn db ************************************************
 # Save/Load configuration related records
-dbLoadRecords("db/saveLoadConfig.db", "P=${AMC_CARRIER_PREFIX}, PORT=${CPSW_PORT}, SAVE_FILE=/tmp/configDump.yaml, LOAD_FILE=yaml/defaultsToro05-21-18.yaml, SAVE_ROOT=mmio, LOAD_ROOT=mmio")
+dbLoadRecords("db/saveLoadConfig.db", "P=${AMC_CARRIER_PREFIX}, PORT=${CPSW_PORT}, SAVE_FILE=/tmp/configDump.yaml, LOAD_FILE=yaml/defaultsToro08-21-18.yaml, SAVE_ROOT=mmio, LOAD_ROOT=mmio")
 
 # Manually create records
 dbLoadRecords("db/bcm.db", "P=${AMC0_PREFIX}, PORT=${CPSW_PORT}, AMC=0")
