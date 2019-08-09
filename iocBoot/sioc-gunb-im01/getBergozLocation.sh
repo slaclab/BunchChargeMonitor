27519e71124d9e11cdb00d11638fd6e6c778485c
Marcio Paduan Donadio <marcio@slac.stanford.edu>
2019-08-09 15:18:41 -0700

    When the script configured the serial port, Bergoz parameters got corrupted. Leonid explained that all the features of this script was used when we had one CPU taking care of many Bergoz. This use case is not needed anymore and, so, the script could be simplified. The misconfiguration issue was fixed with the simplified script.

--- iocBoot/sioc-gunb-im01/getBergozLocation.sh
+++ iocBoot/sioc-gunb-im01/getBergozLocation.sh
@@ -5,8 +5,6 @@
 ##########################################
 
 DEVICE_PATH=/dev/ttyACM*
-SERIAL_NUMBER=$(echo ${IM01} | tr '[:lower:]' '[:upper:]')
-echo $SERIAL_NUMBER
 
 for SERIAL_DEVICE in $DEVICE_PATH ; do
     if [ ! -w "$SERIAL_DEVICE" ]; then
@@ -15,33 +13,9 @@ for SERIAL_DEVICE in $DEVICE_PATH ; do
     fi
 
     DEVICE=$SERIAL_DEVICE
-    # start 'cat' process and save the PID
-    cat $SERIAL_DEVICE >> /data/Reterived_Serial &
-    CAT_PID=$!
 
-    # configure serial port
-    stty -F $SERIAL_DEVICE raw pass8 echo -crtscts cstopb
-
-    # get register values
-    echo -en "S0?\n\0" > $SERIAL_DEVICE
-
-    # stop the 'cat' process by PID
-    # wait for the 5 seconds to fill the file
-    sleep 5
-    kill $CAT_PID
-
-    # Get one serial number from file
-    FOUND_DEVICE=$(cat /data/Reterived_Serial | grep -iE -m 1 'S0:.{4}=.{8}')
-    echo 'Found Serial Number ' $FOUND_DEVICE
-
-    case $FOUND_DEVICE in
-        *"$SERIAL_NUMBER"*)
-            echo 'Making enviroment Variable for path'
-            echo "epicsEnvSet(IM01_PATH,${DEVICE})"
-            echo "epicsEnvSet(IM01_PATH,${DEVICE})" > /data/im01_path
-        ;;
-    esac
-
-    rm /data/Reterived_Serial
+    echo 'Making environment Variable for path'
+    echo "epicsEnvSet(IM01_PATH,${DEVICE})"
+    echo "epicsEnvSet(IM01_PATH,${DEVICE})" > /tmp/im01_path
 done
 exit
