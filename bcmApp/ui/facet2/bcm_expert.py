1e139ef1dc7f60225700dea49d8a1c2eddb44977
rreno <rreno@slac.stanford.edu>
2020-12-02 10:40:52 -0800

    Added pydm ui files for LCLS-MR, LCLS-II, and FACET-II

---
+++ bcmApp/ui/facet2/bcm_expert.py
@@ -0,0 +1,24 @@
+import os
+from pydm import Display
+
+class BCMExpert(Display):
+
+    def __init__(self, parent=None, args=None, macros=None):
+        super(BCMExpert, self).__init__(parent=parent, args=args, macros=macros)
+        self.setup_plots()
+
+    def setup_plots(self):
+        self.ui.wfpRawPlusWeight.plotItem.setLabels(
+        left="ADC counts",
+        bottom="nanoseconds")
+
+        self.ui.wfpRawTimesWeight.plotItem.setLabels(
+        left="ADC counts",
+        bottom="nanoseconds")
+
+    def ui_filename(self):
+        return "bcm_expert.ui"
+
+    def ui_filepath(self):
+        ui_path = os.path.dirname(os.path.realpath(__file__))
+        return os.path.join(ui_path, self.ui_filename())
