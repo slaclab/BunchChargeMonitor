b3fcbf13a030a6d3bba671b39ba72421c0d6f507
rreno <rreno@slac.stanford.edu>
2020-12-03 13:53:38 -0800

    FACET-II: Added AMC1 device to sioc-in10-im01

--- iocBoot/sioc-in10-im01/st.cmd
+++ iocBoot/sioc-in10-im01/st.cmd
@@ -15,7 +15,7 @@
 #            ENVIRONMENT VARIABLES
 # ===========================================
 
-epicsEnvSet("CPSW_PORT","Atca7")
+epicsEnvSet("CPSW_PORT","ATCA5")
 
 # Yaml File
 epicsEnvSet("YAML_DIR", "$(IOC_DATA)/$(IOC)/yaml"
@@ -32,11 +32,14 @@ epicsEnvSet("FPGA_IP", "10.1.1.105")
 epicsEnvSet("AUTO_GEN", 0)
 
 epicsEnvSet("AREA","IN10")
-epicsEnvSet("UNIT","362")
+epicsEnvSet("AMC0_POS","362")
+epicsEnvSet("AMC1_POS", "591")
 epicsEnvSet("IOC_UNIT", "IM01")
 
-# BCM-TORO in crate 2, slot 5, AMC 0
-epicsEnvSet("AMC0_PREFIX","TORO:$(AREA):$(UNIT)")
+# BCM-TORO in crate 2, slot 5, AMC 0 (IM10431)
+epicsEnvSet("AMC0_PREFIX","TORO:$(AREA):$(AMC0_POS)")
+# BCM-TORO in crate 2, slot 5, AMC 1 (IM10591)
+epicsEnvSet("AMC1_PREFIX", "TORO:$(AREA):$(AMC1_POS)")
 
 # AMCC in crate 2, slot 5
 epicsEnvSet("AMC_CARRIER_PREFIX","AMCC:$(AREA):$(UNIT)")
@@ -45,15 +48,12 @@ epicsEnvSet("AMC_CARRIER_PREFIX","AMCC:$(AREA):$(UNIT)")
 epicsEnvSet("DICT_FILE", "yaml/bcmMR.dict")
 
 # Start up enviroment variable 
-epicsEnvSet("STARTUP","/usr/local/lcls/epics/iocCommon/${IOC_NAME}")
-
-# BPM used for BPM Sync
-epicsEnvSet("BPM_LOCA", 314)
+epicsEnvSet("STARTUP","/usr/local/lcls/epics/iocCommon/$(IOC_NAME)")
 
 # IOC Admin
 epicsEnvSet(IOC_NAME,"SIOC:$(AREA):$(IOC_UNIT)")
 
-cd ${TOP}
+cd $(TOP)
 
 # ===========================================
 #               DBD LOADING
@@ -86,7 +86,7 @@ cpswLoadConfigFile("$(YAML_CONFIG_FILE)", "mmio", "")
 #    Record name Prefix,
 #    Use DB Autogeneration,     # Set to 1 for autogeneration of records from the YAML definition. Set to 0 to disable it
 #    Load dictionary,           # Dictionary file path with registers to load. An empty string will disable this function
-YCPSWASYNConfig("$(CPSW_PORT)", "", "", "$(AUTO_GEN)", "${DICT_FILE}")
+YCPSWASYNConfig("$(CPSW_PORT)", "", "", "$(AUTO_GEN)", "$(DICT_FILE)")
 
 
 tprTriggerAsynDriverConfigure("trig", "mmio/AmcCarrierCore")
@@ -100,13 +100,19 @@ tprPatternAsynDriverConfigure("pattern", "mmio/AmcCarrierCore", "tstream")
 # ===========================================
 
 #Save/Load configuration related records
-dbLoadRecords("db/saveLoadConfig.db", "P=${AMC_CARRIER_PREFIX}, PORT=${CPSW_PORT}")
+dbLoadRecords("db/saveLoadConfig.db", "P=$(AMC_CARRIER_PREFIX), PORT=$(CPSW_PORT)")
 
 # Manually create records
 dbLoadRecords("db/bcmFACET2.db", "P=$(AMC0_PREFIX), PORT=$(CPSW_PORT), AMC=0, CHAN=0")
+dbLoadRecords("db/bcmFACET2.db", "P=$(AMC1_PREFIX), PORT=$(CPSW_PORT), AMC=1, CHAN=0")
+
 dbLoadRecords("db/carrierFACET2.db", "P=$(AMC_CARRIER_PREFIX), PORT=$(CPSW_PORT)")
+
 dbLoadRecords("db/waveform.db", "P=$(AMC0_PREFIX)")
+dbLoadRecords("db/waveform.db", "P=$(AMC1_PREFIX)")
+
 dbLoadRecords("db/streamControl.db", "P=$(AMC0_PREFIX)")
+dbLoadRecords("db/streamControl.db", "P=$(AMC1_PREFIX)")
 
 dbLoadRecords("db/iocMeta.db", "AREA=$(AREA), IOC_UNIT=$(IOC_UNIT)")
 
@@ -134,31 +140,39 @@ dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,
 dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=14,DEV_PREFIX=$(AMC0_PREFIX):TRG14:,PORT=trig")
 dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=15,DEV_PREFIX=$(AMC0_PREFIX):TRG15:,PORT=trig")
 
-# ************************************************************************
-# **** Load BSA driver DB ************************************************
-#dbLoadRecords("db/bsa.db", "DEV=$(AMC0_PREFIX),PORT=bsaPort,BSAKEY=CHRG,SECN=CHRG")
-#dbLoadRecords("db/bsa.db", "DEV=$(AMC0_PREFIX),PORT=bsaPort,BSAKEY=CHRGUNC,SECN=CHRGUNC")
-#dbLoadRecords("db/bsa.db", "DEV=$(AMC0_PREFIX),PORT=bsaPort,BSAKEY=RAWSUM,SECN=RAWSUM")
-#dbLoadRecords("db/bsa.db", "DEV=$(AMC0_PREFIX),PORT=bsaPort,BSAKEY=CHRGFLOAT,SECN=CHRGFLOAT")
-#dbLoadRecords("db/bsa.db", "DEV=$(AMC0_PREFIX),PORT=bsaPort,BSAKEY=TOROSTATUS,SECN=TOROSTATUS")
-#dbLoadRecords("db/bsa.db", "DEV=$(AMC0_PREFIX),PORT=bsaPort,BSAKEY=CHRGNOTMIT,SECN=CHRGNOTMIT")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=00,DEV_PREFIX=$(AMC1_PREFIX):TRG00:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=01,DEV_PREFIX=$(AMC1_PREFIX):TRG01:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=02,DEV_PREFIX=$(AMC1_PREFIX):TRG02:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=03,DEV_PREFIX=$(AMC1_PREFIX):TRG03:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=04,DEV_PREFIX=$(AMC1_PREFIX):TRG04:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=05,DEV_PREFIX=$(AMC1_PREFIX):TRG05:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=06,DEV_PREFIX=$(AMC1_PREFIX):TRG06:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=07,DEV_PREFIX=$(AMC1_PREFIX):TRG07:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=08,DEV_PREFIX=$(AMC1_PREFIX):TRG08:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=09,DEV_PREFIX=$(AMC1_PREFIX):TRG09:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=10,DEV_PREFIX=$(AMC1_PREFIX):TRG10:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=11,DEV_PREFIX=$(AMC1_PREFIX):TRG11:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=12,DEV_PREFIX=$(AMC1_PREFIX):TRG12:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=13,DEV_PREFIX=$(AMC1_PREFIX):TRG13:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=14,DEV_PREFIX=$(AMC1_PREFIX):TRG14:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=0,SYS=SYS0,NN=15,DEV_PREFIX=$(AMC1_PREFIX):TRG15:,PORT=trig")
 
 
 # ************************************************************************
 # **** Load iocAdmin databases to support IOC Health and monitoring ******
-dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC_NAME}")
-dbLoadRecords("db/iocAdminScanMon.db","IOC=${IOC_NAME}")
+dbLoadRecords("db/iocAdminSoft.db","IOC=$(IOC_NAME)")
+dbLoadRecords("db/iocAdminScanMon.db","IOC=$(IOC_NAME)")
 
 # The following database is a result of a python parser
 # which looks at RELEASE_SITE and RELEASE to discover
 # versions of software your IOC is referencing
 # The python parser is part of iocAdmin
-dbLoadRecords("db/iocRelease.db","IOC=${IOC_NAME}")
+dbLoadRecords("db/iocRelease.db","IOC=$(IOC_NAME)")
 
 
 # ***********************************************************************
 # **** Load database for autosave status ********************************
-dbLoadRecords("db/save_restoreStatus.db", "P=${IOC_NAME}:")
+dbLoadRecords("db/save_restoreStatus.db", "P=$(IOC_NAME):")
 
 # ===========================================
 #           SETUP AUTOSAVE/RESTORE
@@ -174,15 +188,15 @@ save_restoreSet_DatedBackupFiles(1)
 # Where to find the list of PVs to save
 # Where "/data" is an NFS mount point setup when linuxRT target
 # boots up.
-set_requestfile_path("/data/${IOC}/autosave-req")
+set_requestfile_path("/data/$(IOC)/autosave-req")
 
 # Where to write the save files that will be used to restore
-set_savefile_path("/data/${IOC}/autosave")
+set_savefile_path("/data/$(IOC)/autosave")
 
 # Prefix that is use to update save/restore status database
 # records
 save_restoreSet_UseStatusPVs(1)
-save_restoreSet_status_prefix("${IOC_NAME}:")
+save_restoreSet_status_prefix("$(IOC_NAME):")
 
 ## Restore datasets
 set_pass0_restoreFile("info_settings.sav")
@@ -195,7 +209,7 @@ set_pass1_restoreFile("info_settings.sav")
 # This is required if you use caPutLog.
 # Set access security filea
 # Load common LCLS Access Configuration File
-< ${ACF_INIT}
+< $(ACF_INIT)
 
 
 # ===========================================
@@ -205,10 +219,10 @@ iocInit()
 
 # Turn on caPutLogging:
 # Log values only on change to the iocLogServer:
-caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
+caPutLogInit("$(EPICS_CA_PUT_LOG_ADDR)")
 caPutLogShow(2)
 
-cd("/data/${IOC}/autosave-req")
+cd("/data/$(IOC)/autosave-req")
 iocshCmd("makeAutosaveFiles")
 
 # Start the save_restore task
@@ -218,4 +232,4 @@ iocshCmd("makeAutosaveFiles")
 create_monitor_set("info_positions.req", 5 )
 create_monitor_set("info_settings.req" , 30 )
 
-cd ${TOP}
+cd $(TOP)
