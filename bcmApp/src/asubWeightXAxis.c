6293cec9179125b2e75463a18628c72863099cd0
Marcio Paduan Donadio <marcio@slac.stanford.edu>
2021-02-20 13:32:13 -0800

    First 28 words of the waveform are the header and are not shown in the waveform

--- bcmApp/src/asubWeightXAxis.c
+++ bcmApp/src/asubWeightXAxis.c
@@ -4,12 +4,13 @@
 
 static long calcTimeArray(aSubRecord *pasub) {
     long i;
-    double clkFreq, sum = 0.0, timeNs;
+    double clkFreq, sum, timeNs;
 
     clkFreq = (double)(*(long *)pasub->a);
 
     /* Calculate the time in ns between each sample */
     timeNs = 1000000000.0 / (2.0 * clkFreq);
+    sum = 28 * timeNs;
 
     /* Then make an array with each element a distance of `timeNs` apart */
     for (i = 0; i < pasub->nova; i++) {
