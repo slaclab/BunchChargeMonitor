901836e113c41dbca661fe402c65e2bde3f05c5b
Marcio Paduan Donadio <marcio@slac.stanford.edu>
2021-02-20 19:47:31 -0800

    Fixed databases for the integration window to display proper data

--- bcmApp/src/asubWeightXAxis.c
+++ bcmApp/src/asubWeightXAxis.c
@@ -10,6 +10,7 @@ static long calcTimeArray(aSubRecord *pasub) {
 
     /* Calculate the time in ns between each sample */
     timeNs = 1000000000.0 / (2.0 * clkFreq);
+    // The first 28 samples are the waveform header and must not be considered
     sum = 28 * timeNs;
 
     /* Then make an array with each element a distance of `timeNs` apart */
