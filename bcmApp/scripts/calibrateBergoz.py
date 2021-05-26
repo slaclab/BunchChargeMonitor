c468258e315e372426839c0af0a866adb849953e
TaineEriksen <taine@slac.stanford.edu>
2021-05-26 10:23:44 -0700

    Updated SC help text, bergoz calibration script and changed trigger screen to display mask in hex

--- bcmApp/scripts/calibrateBergoz.py
+++ bcmApp/scripts/calibrateBergoz.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 from epics import PV
 
@@ -15,54 +16,82 @@ def process_args(argv):
 
 def calibrateBergoz(prefix_info):
     #pv's needed
-    trig0=PV("TPR:{}:{}:0:TRG00_SYS2_TCTL".format(prefix_info.AREA, prefix_info.IOC_UNIT))
-    chan0=PV("TPR:{}:{}:0:CH00_SYS2_TCTL".format(prefix_info.AREA, prefix_info.IOC_UNIT))
-    calib=PV("TPR:{}:{}:0:CH06_FIXEDRATE".format(prefix_info.AREA, prefix_info.IOC_UNIT))
-    trig3=PV("TORO:{}:{}:TLR3:SelSrc".format(prefix_info.AREA, prefix_info.POS))
-    trig4=PV("TORO:{}:{}:TLR4:SelSrc".format(prefix_info.AREA, prefix_info.POS))
-    trig5=PV("TORO:{}:{}:TLR5:SelSrc".format(prefix_info.AREA, prefix_info.POS))
-    trig7=PV("TORO:{}:{}:TLR7:SelSrc".format(prefix_info.AREA, prefix_info.POS))
+    trig0TCTL=PV("TPR:{}:{}:0:TRG00_SYS2_TCTL".format(prefix_info.AREA, prefix_info.IOC_UNIT))
+    chan0TCTL=PV("TPR:{}:{}:0:CH00_SYS2_TCTL".format(prefix_info.AREA, prefix_info.IOC_UNIT))
+    chan6Rate=PV("TPR:{}:{}:0:CH06_FIXEDRATE".format(prefix_info.AREA, prefix_info.IOC_UNIT))
+    calibEnable=PV("TORO:{}:{}:CalibEnable".format(prefix_info.AREA, prefix_info.IOC_UNIT))
+    trig3SelSrc=PV("TORO:{}:{}:TLR3:SelSrc".format(prefix_info.AREA, prefix_info.POS))
+    trig4SelSrc=PV("TORO:{}:{}:TLR4:SelSrc".format(prefix_info.AREA, prefix_info.POS))
+    trig5SelSrc=PV("TORO:{}:{}:TLR5:SelSrc".format(prefix_info.AREA, prefix_info.POS))
+    trig7SelSrc=PV("TORO:{}:{}:TLR7:SelSrc".format(prefix_info.AREA, prefix_info.POS))
     
-    print("Setting Calibration Mode")
+    print("Calibration Starting")
     
-    trig0_sav = trig0.get()
-    chan0_sav = chan0.get()
-    calib_sav = calib.get()
-    trig3_sav = trig3.get()
-    trig4_sav = trig4.get()
-    trig5_sav = trig5.get()
-    trig7_sav = trig7.get()   
+    trig0TCTL_sav = trig0TCTL.get()
+    chan0TCTL_sav = chan0TCTL.get()
+    chan6Rate_sav = chan6Rate.get()
+    calibEnable_sav = calibEnable.get()
+    trig3SelSrc_sav = trig3SelSrc.get()
+    trig4SelSrc_sav = trig4SelSrc.get()
+    trig5SelSrc_sav = trig5SelSrc.get()
+    trig7SelSrc_sav = trig7SelSrc.get()   
     
     #need to check when the ioc is back online that these are correctly getting gathered
-    print(trig0_sav)
-    print(chan0_sav)
-    print(calib_sav)
-    print(trig3_sav)
-    print(trig4_sav)
-    print(trig5_sav)
-    print(trig7_sav)
+    #print(trig0TCTL_sav)
+    #print(chan0TCTL_sav)
+    #print(chan6Rate_sav)
+    #print(calibEnable_sav)
+    #print(trig3SelSrc_sav)
+    #print(trig4SelSrc_sav)
+    #print(trig5SelSrc_sav)
+    #print(trig7SelSrc_sav)
     
     #check these are the correct possitions once the ioc is back online
-    trig0.put(0)
-    chan0.put(0)
-    calib.put(1)
-    trig3.put(2)
-    trig4.put(2)
-    trig5.put(2)
-    trig7.put(2)
+    trig0TCTL.put(0,timeout=10.0)
+    chan0TCTL.put(0,timeout=10.0)
+    chan6Rate.put(6,timeout=10.0)
+    calibEnable.put(1,timeout=10.0)
+    trig3SelSrc.put(2,timeout=10.0)
+    trig4SelSrc.put(2,timeout=10.0)
+    trig5SelSrc.put(2,timeout=10.0)
+    trig7SelSrc.put(2,timeout=10.0)
+    
+    #print("after setting calibration")
+    #print(trig0TCTL.get())
+    #print(chan0TCTL.get())
+    #print(chan6Rate.get())
+    #print(calibEnable.get())
+    #print(trig3SelSrc.get())
+    #print(trig4SelSrc.get())
+    #print(trig5SelSrc.get())
+    #print(trig7SelSrc.get())
+    
+    print("Waiting for calibration to occur")
+    
+    time.sleep(5)
     
     print("Calibration Complete")
     
     
-    print("Reseting to PV's to there Original Possition")
+    print("Reseting to PV's")
     
-    trig0.put(trig0_sav)
-    chan0.put(chan0_sav)
-    calib.put(calib_sav)
-    trig3.put(trig3_sav)
-    trig4.put(trig4_sav)
-    trig5.put(trig5_sav)
-    trig7.put(trig7_sav)
+    trig0TCTL.put(trig0TCTL_sav,timeout=10.0)
+    chan0TCTL.put(chan0TCTL_sav,timeout=10.0)
+    chan6Rate.put(chan6Rate_sav,timeout=10.0)
+    calibEnable.put(calibEnable_sav,timeout=10.0)
+    trig3SelSrc.put(trig3SelSrc_sav,timeout=10.0)
+    trig4SelSrc.put(trig4SelSrc_sav,timeout=10.0)
+    trig5SelSrc.put(trig5SelSrc_sav,timeout=10.0)
+    trig7SelSrc.put(trig7SelSrc_sav,timeout=10.0)
+    
+    #print("After reseting to original possitions")
+    #print(trig0TCTL.get())
+    #print(chan0TCTL.get())
+    #print(calibEnable.get())
+    #print(trig3SelSrc.get())
+    #print(trig4SelSrc.get())
+    #print(trig5SelSrc.get())
+    #print(trig7SelSrc.get())
 
 def main(argv):
     pv_data = process_args(argv)
--- bcmApp/ui/bcm_main.py
+++ bcmApp/ui/bcm_main.py
@@ -707,51 +707,18 @@ class BCMExpert(Display):
         lbl1 = QLabel("<b>Explanation of Averaging window</b>")
         lbl1.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
 
-        lbl2 = QLabel("This text is not accurate and needs updating "\
-                      "You use the pre and post regions to select the region "\
-                      "where the signal is and the region where the background "\
-                      "noise is. The reason for this is to subtract the noise "\
-                      "from the signal. It is up to you to choose if you want "\
-                      "to place the signal in the post or pre regions. The "\
-                      "middle region provides a void area between the other regions. "\
-                      "This area is ignored by the system. Use it if you need a "\
-                      "distinct separation between the pre and post regions.")
+        lbl2 = QLabel("The Bergoz chassis outputs a constant voltage level which"\
+                      " the Bunch Charge Monitoring system converts into a measurement"\
+                      " in pC. To operate, set the averaging window so that it does"\
+                      " include the transition between voltage levels.")
         lbl2.setWordWrap(True)
 
-        lbl3 = QLabel("The idea with the edges and coefficients is summarized by "\
-                      "this formula:<br><br>"\
-                      "sum(pre region) * CoefA0 + sum(post region) * CoefA1 + Offset<br>")
-        lbl3.setWordWrap(True)
-
-        lbl4 = QLabel("The pre and post regions selected using the sliders will "\
-                      "be integrated separately. Coefficient A0 will be "\
-                      "multiplied by the pre region and coefficient A1 will be "\
-                      "multiplied by the post region. Both regions are finnaly "\
-                      "summed up and the result is changed by an offset. You must "\
-                      "choose opposite signals for A0 and A1 to make the noise "\
-                      "be subtracted from the signal. Otherwise you will sum them up.")
-        lbl4.setWordWrap(True)
-
-        lbl5 = QLabel("Attention with the start of each region. The pre region "\
-                      "starts at time 0, as well as the middle region. So, the "\
-                      "pre and middle regions overlap with each other. If the "\
-                      "middle region is smaller than the pre region, the system "\
-                      "just ignores the middle region. The post region is"\
-                      "different. It starts where the pre or middle region stops,"\
-                      "whichever is bigger. This chart may make things more clear:")
-        lbl5.setWordWrap(True)
-
-        svg = QSvgWidget("help.svg", parent=dlg)
         lo.addWidget(lbl1)
         lo.addWidget(lbl2)
-        lo.addWidget(lbl3)
-        lo.addWidget(lbl4)
-        lo.addWidget(lbl5)
-        lo.addWidget(svg)
 
-        dlg.setMinimumSize(600, 600)
+        dlg.setMinimumSize(500, 200)
         dlg.setLayout(lo)
-        dlg.setWindowTitle("BCM Calibration Help")
+        dlg.setWindowTitle("BCM Help")
         dlg.setModal(False)
 
         # if there's already a dialog open we should close it
