b348f6ea34c369eb7f37f30a6eda84be068653e3
TaineEriksen <taine@slac.stanford.edu>
2021-07-12 11:32:55 -0700

    Added the ability to invert the coeficents

--- bcmApp/ui/launch_im10431.sh
+++ bcmApp/ui/launch_im10431.sh
@@ -1,3 +1,3 @@
 source $TOOLS/script/use_pydm.sh
 export PYDM_DISPLAYS_PATH=$TOOLS/pydm/display/
-pydm --hide-nav-bar -m "AREA=IN10,POS=431,UNIT=1,INST=0,CHAN=IM10431,IOC_UNIT=IM02,IOC=sioc-b084-im02, CPU=cpu-b084-sp17, CRATE=shm-b084-sp17-2" bcm_main.py & 
+pydm --hide-nav-bar -m "AREA=IN10,POS=431,UNIT=1,INST=0,CHAN=IM10431,IOC_UNIT=IM02,IOC=sioc-b084-im02, CPU=cpu-b084-sp17, CRATE=shm-b084-sp17-2, TYPE=TORO" bcm_main.py & 
--- bcmApp/ui/launch_im10591.sh
+++
@@ -1,3 +0,0 @@
-source $TOOLS/script/use_pydm.sh
-export PYDM_DISPLAYS_PATH=$TOOLS/pydm/display/
-pydm --hide-nav-bar -m "AREA=IN10,POS=591,UNIT=1,INST=0,CHAN=IM10591,IOC_UNIT=IM02,IOC=sioc-b084-im02, CPU=cpu-b084-sp17, CRATE=shm-b084-sp17-2" bcm_main.py & 
