96fa01c1cc9f9117cad031bfcdb497e79f89391e
TaineEriksen <taine@slac.stanford.edu>
2021-09-14 17:10:09 -0700

    Inital commit with calibration code

---
+++ bcmApp/scripts/calibrateBergoz.py
@@ -0,0 +1,5 @@
+#Autor          : Taine Eriksen
+#Created        : 08/31/2021
+#Description    : A converstion of the matlab calibration script from Leonid Sapozhnikov to provide functionality on the display
+
+
