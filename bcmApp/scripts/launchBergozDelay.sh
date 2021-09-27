4dd2a3a6083ae2278443fcbb5f4bf0ba1593f35e
TaineEriksen <taine@slac.stanford.edu>
2021-09-27 15:24:16 -0700

    Corrected launch scrip to work on PROD

--- bcmApp/scripts/launchBergozDelay.sh
+++ bcmApp/scripts/launchBergozDelay.sh
@@ -1,3 +1,11 @@
-source /afs/slac/g/lcls/package/anaconda/envs/python3.7env/bin/activate;
+if [ hostname = "lcls-dev3" ]
+then 
+    source /afs/slac/g/lcls/package/anaconda/envs/python3.7env/bin/activate
+fi
+
+if [ hostname = "lcls-srv01" ]
+then
+    source /usr/local/lcls/package/anaconda/envs/python3.7env/bin/activate
+fi
 export PYTHONPATH=/afs/slac/g/lcls/tools/python/toolbox;
 python $TOOLS/script/setBergozDelay.py $1 $2 $3 $4
