6a5ee66038b2f0f893a09558b9a40e179a0c5d0d
TaineEriksen <taine@slac.stanford.edu>
2021-10-20 08:51:52 -0700

    Readded temprature IOC for sector 4

---
+++ iocBoot/sioc-emit2-im02/st.cmd
@@ -0,0 +1,19 @@
+#!../../bin/linuxRT-x86_64/bcm
+
+#- You may have to change bcm to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+# Production area
+epicsEnvSet("AREA","EMIT2")
+
+# Start up enviroment variable 
+epicsEnvSet("STARTUP","/afs/slac/g/lcls/epics/iocCommon/${IOC_NAME}")
+
+# ***********************************************************************
+# **** Environment variables for IOC Admin ******************************
+epicsEnvSet(IOC_NAME,"SIOC:$(AREA):IM02")
+
+cd $(TOP)
+< iocBoot/common/temperatureMonitor.cmd
