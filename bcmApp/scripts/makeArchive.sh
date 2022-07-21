396744dee4ddcabbd1ee7080e5dc66575a8beeba
Kyle Leleux <kleleux@slac.stanford.edu>
2022-07-21 12:19:21 -0700

    Fixed another bug in archiver.

--- bcmApp/scripts/makeArchive.sh
+++ bcmApp/scripts/makeArchive.sh
@@ -7,6 +7,6 @@ do
     sleep 1    
 done
 
-cat $IOC_DATA/$1/iocInfo/IOC.pvlist | grep -v "wave" | grep -v string | grep -v subArray | grep -v sub | grep -v bsa | grep -v compress | grep -v fanout | grep -v seq | cut -f1 -d "," | sed 's/$/ 10 scan/' > $IOC_DATA/$1/archive/MAIN.archive
+cat $IOC_DATA/$1/iocInfo/IOC.pvlist | grep -v "wave" | grep -v 'BSSSCHNMASK\|BSSSCHNSEVR\|SCUD1\|PIDSCUD1\|SCUD2\|PIDSCUD2\|SCUD3\|PIDSCUD3\|SCS1H\|PIDSCS1H\|SCSTH\|PIDSCSTH\|SCSHH\|PIDSCSHH\|SCH1H\|PIDSCH      1H\|SCHTH\|PIDSCHTH\|SCHHH\|PIDSCHHH' | grep -v string | grep -v subArray | grep -v sub | grep -v bsa | grep -v compress | grep -v fanout | grep -v seq | cut -f1 -d "," | sed 's/$/ 10 scan/' > $IOC_DATA/$1/archive/MAIN.archive
 cat $IOC_DATA/$1/iocInfo/IOC.pvlist | grep bsa | grep 1H | cut -f1 -d "," | sed 's/$/ 1 scan/' > $IOC_DATA/$1/archive/LCLS1BSA.archive
-cat $IOC_DATA/sioc-sph-im01/iocInfo/IOC.pvlist | grep 'BSSSCHNMASK\|BSSSCHNSEVR\|SCUD1\|PIDSCUD1\|SCUD2\|PIDSCUD2\|SCUD3\|PIDSCUD3\|SCS1H\|PIDSCS1H\|SCSTH\|PIDSCSTH\|SCSHH\|PIDSCSHH\|SCH1H\|PIDSCH1H\|SCHTH\|PIDSCHTH\|SCHHH\|PIDSCHHH' |  grep 'CHRG\|CHRGUNC\|RAWSUM\|CHRGFLOAT\|TOROSTATUS\|CHRGNOTMIT' |  grep 'bo\|mbbo\|ai\|int64in' | cut -f1 -d "," | sed 's/$/ 1 scan/' > $IOC_DATA/$1/archive/bsss.archive
+cat $IOC_DATA/$1/iocInfo/IOC.pvlist | grep 'BSSSCHNMASK\|BSSSCHNSEVR\|SCUD1\|PIDSCUD1\|SCUD2\|PIDSCUD2\|SCUD3\|PIDSCUD3\|SCS1H\|PIDSCS1H\|SCSTH\|PIDSCSTH\|SCSHH\|PIDSCSHH\|SCH1H\|PIDSCH1H\|SCHTH\|PIDSCHTH\|SCHHH\|PIDSCHHH' |  grep 'CHRG\|CHRGUNC\|RAWSUM\|CHRGFLOAT\|TOROSTATUS\|CHRGNOTMIT' |  grep 'bo\|mbbo\|ai\|int64in' | cut -f1 -d "," | sed 's/$/ 1 scan/' > $IOC_DATA/$1/archive/bsss.archive
