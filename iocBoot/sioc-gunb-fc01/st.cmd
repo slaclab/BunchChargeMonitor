9bf69b1c8d7eb178c93e6ce23d64c782daff8edc
Kyle Leleux <kleleux@slac.stanford.edu>
2022-09-15 15:30:16 -0700

    Removed :0: from faraday cup pvs as well.

--- iocBoot/sioc-gunb-fc01/st.cmd
+++ iocBoot/sioc-gunb-fc01/st.cmd
@@ -187,9 +187,9 @@ dbLoadRecords("db/saveLoadConfig.db", "P=${AMC_CARRIER_PREFIX}, PORT=${CPSW_PORT
 
 # Manually create records
 dbLoadRecords("db/bcmAmc.db", "P=$(AMC0_PREFIX), PORT=$(CPSW_PORT), AMC=0")
-dbLoadRecords("db/bcmChan.db", "P=$(AMC0_PREFIX):0, PORT=$(CPSW_PORT), AMC=0, CHAN=0")
+dbLoadRecords("db/bcmChan.db", "P=$(AMC0_PREFIX), PORT=$(CPSW_PORT), AMC=0, CHAN=0")
 dbLoadRecords("db/bcmLCLS2amc.db", "P=$(AMC0_PREFIX), PORT=$(CPSW_PORT), AMC=0")
-dbLoadRecords("db/bcmLCLS2chan.db", "P=$(AMC0_PREFIX):0, PORT=$(CPSW_PORT), AMC=0, CHAN=0")
+dbLoadRecords("db/bcmLCLS2chan.db", "P=$(AMC0_PREFIX), PORT=$(CPSW_PORT), AMC=0, CHAN=0")
 
 dbLoadRecords("db/carrier.db", "P=${AMC_CARRIER_PREFIX}, PORT=${CPSW_PORT}")
 
@@ -215,8 +215,8 @@ dbLoadRecords("db/processRawWFHeader.db", "P=$(AMC0_PREFIX),CHAN=0")
 dbLoadRecords("db/carrierLCLS2.db", "P=${AMC_CARRIER_PREFIX}, PORT=${CPSW_PORT}")
 
 #coeficent calibraiton
-dbLoadRecords("db/farady_cup_coef_calib.db", "P=${AMC_CARRIER_PREFIX}, AMC=0")
-dbLoadRecords("db/coefficientScaling.db", "P=${AMC_CARRIER_PREFIX}, AMC=0")
+dbLoadRecords("db/farady_cup_coef_calib.db", "P=${AMC_CARRIER_PREFIX}")
+dbLoadRecords("db/coefficientScaling.db", "P=${AMC_CARRIER_PREFIX}")
 
 # Allow time for Keithley driver to connect
 epicsThreadSleep(1.0)
